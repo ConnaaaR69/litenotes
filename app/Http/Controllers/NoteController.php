0c710d985ba3e51f5cac3d2f5490e86384074cc5
ConnaaaR69 <connormattless@gmail.com>
2022-10-07 09:52:40 +0100

    index shows all notes

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * 
+     * @return \Illuminate\Http\Response
+    */
+    public function index()
+    {
+        //Fetch notes in order of when they were last updated. Latest First.
+        $notes = Note::where('user_id', Auth::id())->latest('updated_at')->paginate(1);
+
+        return view('index')->with('notes', $notes);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * 
+     * @return \Iluminate\Http\Response
+     */
+    public function create() 
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) 
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     * 
+     * @param int $id
+     * @return \Iluminate\Http\Response
+     */
+    public function show($id)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     * 
+     * @param int $id
+     * @return \Iluminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * 
+     * @param \Iluminate\Http\Request $request
+     * @param int $id
+     * @return \Iluminate\Http\Response
+     */
+
+     public function update(Request $request, $id)
+     {
+
+     }
+    
+    /**
+     * Remove the specified resource from storage.
+     * 
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+     public function destroy($id)
+     {
+
+     }
+}   
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/notes', [NoteController::class, 'index']);
+Route::get('/notes/create', [NoteController::class, 'create']);
+Route::post('/notes', [NoteController::class, 'store']);
+
+Route::resource('/notes', NoteController::Class);
+Route::resource('/notes', NoteController::Class)->middleware(['auth']);
 
 Route::get('/', function () {
     return view('welcome');
